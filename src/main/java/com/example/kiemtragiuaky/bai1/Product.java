9e95ff3a7291af31a3539c6be5db8aabac703a6c
T14 <vykid9@gmail.com>
2024-12-04 13:32:01 +0700

    Cau1

---
+++ src/main/java/com/example/kiemtragiuaky/bai1/Product.java
@@ -0,0 +1,47 @@
+package com.example.kiemtragiuaky.bai1;
+
+public class Product {
+    private String name;
+    private double price;
+    private String description;
+    private String urlImage;
+
+    public Product(String name, double price, String description, String urlImage) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.urlImage = urlImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
